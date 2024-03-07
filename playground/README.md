d1203c493e00409b677383c4772b30c93b76103f
indamutsa <arsichizy@gmail.com>
2024-03-07 16:42:40 +0100

    added bcrypt to the mix

--- playground/README.md
+++ playground/README.md
@@ -1,1 +1,5 @@
+In this playground, we will hash and validate the password using bcrypt. We would like to ensure that the password is hashed and stored securely in the database. We will use mongo pre-hook savers to hash the password before saving it to the database and then bcrypt compare method to validate the password.
 
+```bash
+npm install bcrypt
+```
--- playground/server/models/UserModel.js
+++ playground/server/models/UserModel.js
@@ -1,6 +1,7 @@
 // First we have to bring in mongoose
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 // Here we define the schema for our users
 const userSchema = mongoose.Schema(
@@ -41,6 +42,29 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Thi function adds a method that hash the password
+async function generateHash(password) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+}
+
+// Pre-save hook to hash the password before saving the user to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await generateHash(user.password).catch((err) => {
+      console.error(err);
+      return next(err);
+    });
+  }
+  return next();
+});
+
+// Method to compare the password for the user
+userSchema.methods.comparePassword = async (candidatePassword) => {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 // We export the model `User` from the `UserSchema`
 module.exports = mongoose.model('User', userSchema);
